70c4d34977662495745d193300c372c8100ad0ce
Ramón Serrano <ramon.calle.88@gmail.com>
2015-04-16 17:24:35 -0430

    Arreaglados problemas de Responsive.

--- js/custom.js
+++ js/custom.js
@@ -20,15 +20,36 @@
         });
         $('.slide-item-category-carousel').owlCarousel({
             navText: ['<i class="fa fa-2 fa-chevron-left"></i>', '<i class="fa fa-2 fa-chevron-right"></i>'],
+            loop: false,
             margin: 50,
-            items: 3,
-            mouseDrag: false,
-            touchDrag: false,
-            pullDrag: false,
-            freeDrag: false,
-            navigation: true, // Show next and prev buttons
             responsiveClass: true,
-            slideSpeed: 1000
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true,
+                    loop: false
+                },
+                600: {
+                    items: 2,
+                    nav: true,
+                    loop: false
+                },
+                1000: {
+                    items: 3,
+                    nav: true,
+                    loop: false
+                },
+                1280: {
+                    items: 3,
+                    nav: true,
+                    loop: false
+                },
+                1600: {
+                    items: 3,
+                    nav: true,
+                    loop: false
+                }
+            }
         });
     });
 })(jQuery);
